69ad64d250e2096c225b88459623b1c96d6ea5a1
PabloBautistaGoyoneche <pablo.bautista.mkt@gmail.com>
2024-10-07 23:18:36 -0500

    fix: updating routes

--- public/index.php
+++ public/index.php
@@ -1,11 +1,29 @@
+<?php
+
+// Incluir el archivo de enrutamiento
+require_once 'router.php';
+
+// Obtener la ruta actual
+$requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+// Llamar a la función de enrutamiento
+route($requestUri);
+
+?>
+
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home Manager</title>
 </head>
+
 <body>
-    
+
+    <?php echo "Bienvenido a HomeManager!";?>
+
 </body>
+
 </html>
---
+++ public/router.php
@@ -0,0 +1,24 @@
+<?php
+
+// Función para manejar las rutas
+function route($path) {
+    switch ($path) {
+        case '/':
+            require __DIR__ . '/index.php';
+            break;
+        case '/propiedades':
+            require __DIR__ . '/properties.php';
+            break;
+        case '/propiedad':
+            require __DIR__ . '/property.php';
+            break;
+        case '/contacto':
+            require __DIR__ . '/contact.php';
+            break;
+        default:
+            // Página 404 si la ruta no existe
+            http_response_code(404);
+            require __DIR__ . '/404.php';
+            break;
+    }
+}
