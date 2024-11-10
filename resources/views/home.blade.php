9743e19a08c867ccfef9656f273339c9198dc0d4
PabloBautistaGoyoneche <pablo.bautista.mkt@gmail.com>
2024-11-10 11:22:59 -0500

    feat:  preparing laravel to use filament

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agente Inmobiliario Rafael Pereyra - Remax Perú</title>
+</head>
+<body>
+    <h1>Inicio</h1>
+    
+</body>
+</html>
